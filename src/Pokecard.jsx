cfd3227948c334d714a39f6d34bb66dd2477b19a
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-08-07 00:57:30 -0400

    add key to li

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -8,7 +8,7 @@ const Pokecard = ({}) => {
     <>
 <div className="Pokecard-div">
   <ul className="Pokecard-list">
-    {pokemons.map(p=> <li className="Pokecard-list-item">
+    {pokemons.map(p=> <li key={p.id} className="Pokecard-list-item">
      <p className="Pokecard-name">{p.name}</p> 
      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} alt="pokemon" />
      <p>Type: {p.type}</p>
